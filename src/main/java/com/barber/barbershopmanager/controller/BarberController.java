8afaca697f9519743dab202752de261c775fb806
rocha101 <eduardoprudenciorocha@gmail.com>
2023-08-11 15:53:27 -0300

    detailing + editing

--- src/main/java/com/barber/barbershopmanager/controller/BarberController.java
+++ src/main/java/com/barber/barbershopmanager/controller/BarberController.java
@@ -3,7 +3,10 @@ package com.barber.barbershopmanager.controller;
 import com.barber.barbershopmanager.model.Barber;
 import com.barber.barbershopmanager.service.BarberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/barbers")
@@ -23,5 +26,29 @@ public class BarberController {
     @PostMapping
     public Barber createBarber(@RequestBody Barber newBarber) {
         return barberService.createBarber(newBarber);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Barber> getBarberById(@PathVariable Long id) {
+        Optional<Barber> barber = barberService.getBarberById(id);
+
+        return barber.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Barber> updateBarber(@PathVariable Long id, @RequestBody Barber updatedBarber) {
+        Optional<Barber> existingBarber = barberService.getBarberById(id);
+
+        if (existingBarber.isPresent()) {
+            Barber barber = existingBarber.get();
+            barber.setUsername(updatedBarber.getUsername());
+
+
+            Barber savedBarber = barberService.updateBarber(barber);
+            return ResponseEntity.ok(savedBarber);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/barber/barbershopmanager/service/BarberService.java
+++ src/main/java/com/barber/barbershopmanager/service/BarberService.java
@@ -5,24 +5,52 @@ import org.springframework.stereotype.Service;
 
 import com.barber.barbershopmanager.model.Barber;
 import com.barber.barbershopmanager.repositories.BarberRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class BarberService {
 
+    private final BarberRepository barberRepository;
+
     @Autowired
-    private BarberRepository action;
+    public BarberService(BarberRepository barberRepository) {
+        this.barberRepository = barberRepository;
+    }
 
     public Iterable<Barber> getAllBarbers() {
-        return action.findAll();
+        return barberRepository.findAll();
     }
 
     public Barber createBarber(Barber barber) {
-        Barber existingBarber = action.findByEmail(barber.getEmail());
+        Barber existingBarber = barberRepository.findByEmail(barber.getEmail());
 
         if (existingBarber != null) {
             throw new IllegalArgumentException("Email is already in use");
         }
 
-        return action.save(barber);
+        return barberRepository.save(barber);
+    }
+
+    public Optional<Barber> getBarberById(Long id) {
+        return barberRepository.findById(Math.toIntExact(id));
+    }
+
+    @Transactional // Make sure to annotate with @Transactional for database operations
+    public Barber updateBarber(Barber updatedBarber) {
+        Optional<Barber> existingBarberOptional = barberRepository.findById(Math.toIntExact(updatedBarber.getId()));
+
+        if (existingBarberOptional.isPresent()) {
+            Barber existingBarber = existingBarberOptional.get();
+
+            // Update properties of the existing barber with the values from updatedBarber
+            existingBarber.setUsername(updatedBarber.getUsername());
+            // Set other properties as needed
+
+            return barberRepository.save(existingBarber); // Save the changes
+        } else {
+            throw new IllegalArgumentException("Barber not found");
+        }
     }
 }
