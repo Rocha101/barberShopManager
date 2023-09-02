cb782f71be14a20b79dbcef309f40edc55db7f19
Aleson Teixeira <115757820+Aleson00@users.noreply.github.com>
2023-09-02 12:31:38 -0300

    Criação de um começo de produtos.

---
+++ src/main/java/com/barber/barbershopmanager/Produto/Entityid.java
@@ -0,0 +1,13 @@
+package com.barber.barbershopmanager.Produto;
+
+public class Entityid {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/barber/barbershopmanager/Produto/Status.java
@@ -0,0 +1,6 @@
+package com.barber.barbershopmanager.Produto;
+
+public enum Status {
+    DISPONIVEL,
+    INDISPONIVEL
+}
