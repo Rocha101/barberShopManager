b2235b5cf21798c38ebb0c5272855f2ffae4495a
TiagoS4 <tiagosallesmelo@hotmail.com>
2023-12-02 21:34:46 -0300

    Rate-limit adicionado
    Rate-limit adicionado ao projeto.

--- src/main/java/com/barbershop/manager/ManagerApplication.java
+++ src/main/java/com/barbershop/manager/ManagerApplication.java
@@ -3,6 +3,13 @@ package com.barbershop.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
 
 @EnableWebMvc
 @SpringBootApplication
@@ -12,4 +19,24 @@ public class ManagerApplication {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
 
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
+	}
+
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+		redisConfig.setHostName("xxxxx");
+		redisConfig.setPort(6379);
+		redisConfig.setPassword("xxxxx");
+		redisConfig.setUsername("xxxxx");
+		JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfig = JedisClientConfiguration.builder();
+		jedisClientConfig.connectTimeout(Duration.ofSeconds(60));
+		jedisClientConfig.useSsl();
+
+		return new JedisConnectionFactory(redisConfig, jedisClientConfig.build());
+	}
 }
